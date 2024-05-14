df28a5f3e6a5579e3bcf08abdd5373581afd2256
VIPIN P <87920104+Vipi-14@users.noreply.github.com>
2024-05-14 14:23:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from htmlTemplates import css, bot_template, user_template
+import numpy as np
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+
+with open('db/texts.json','r') as jn:
+    texts = json.load(jn)
+
+def get_embeddings(texts):
+    model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+    embeddings = model.encode(texts, convert_to_tensor=True)
+    return embeddings
+
+
+def search_similar_thoughts(user_thought, texts, embeddings):
+    user_embedding = get_embeddings([user_thought])[0]
+    similarities = cosine_similarity([user_embedding], embeddings)[0]
+    sorted_indices = np.argsort(similarities)[::-1]
+    similar_texts = [(texts[i], similarities[i]) for i in sorted_indices]
+    return similar_texts
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Juxta",
+                       page_icon=":thought_balloon:")
+    st.write(css, unsafe_allow_html=True)
+
+    st.header("Connect Your Thoughts :thought_balloon:")
+    user_thought = st.text_input("Enter your thought:")
+    
+    if user_thought:
+        st.write("Searching ...")
+        # st.write("User Thought:", user_thought)
+        embeddings = get_embeddings(texts)
+        
+        similar_texts = search_similar_thoughts(user_thought, texts, embeddings)
+        
+        result = False
+
+        for text, similarity in similar_texts[:4]:
+            # st.write(f"Similarity: {similarity:.2f}")
+            if similarity>0.3:
+                st.write(text)
+                st.write("---")
+                result = True
+        
+        if not result:
+            st.write(f"No similar Thoughts before")
+
+        if st.button("Add to Notes"):
+            texts.append(user_thought)
+            embeddings = get_embeddings(texts)  # Re-embed texts after addition
+            st.write("User thought added successfully!")
+
+if __name__ == '__main__':
+    main()
+    with open('db/texts.json','w') as jn:
+        json.dump(texts,jn,indent=4)
---
+++ readme.md
@@ -0,0 +1,44 @@
+# Juxta :thought_balloon:
+
+Juxta is a Streamlit web application that allows users to connect their thoughts by searching for similar thoughts based on user input and add to the database.
+
+## Overview
+
+Juxta uses SentenceTransformer to encode text into embeddings and then calculates cosine similarity to find similar thoughts from a pre-existing database of texts.
+
+## Features
+
+- **Thought Search**: Users can enter their thought, and the application will search for similar thoughts from the database.
+- **Add to Notes**: Users can add their thought to the database for future searches.
+- **Database Persistence**: The database of texts is stored in a JSON file and is updated dynamically when new thoughts are added.
+
+## Setup
+
+1. Clone the repository:
+
+
+
+## Install dependencies:
+
+pip install -r requirements.txt
+
+## Run the Streamlit app:
+
+streamlit run app.py
+
+## Usage
+
+Enter your thought in the text input field.
+Click on the "Enter" button or press "Enter" on your keyboard to start the search.
+If similar thoughts are found, they will be displayed on the screen.
+If no similar thoughts are found, a message will be displayed indicating so.
+You can click on the "Add to Notes" button to add your thought to the database.
+Database
+The database of texts is stored in a JSON file named texts.json located in the db directory.
+
+## Contributing
+
+Contributions are welcome! If you have any ideas for improvement or would like to fix any issues, feel free to open a pull request.
+
+License
+This project is licensed under the MIT License.
